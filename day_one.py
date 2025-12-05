6f425ad9b9def021c07a07cf14a5aad1967e02b7
carlhagred <carl.hagred@gmail.com>
2025-12-05 11:26:00 +0100

    part two of day one

--- day_one.py
+++ day_one.py
@@ -1,21 +1,41 @@
-dial_position = 50
-secret_code = 0
 input = []
+
 
 with open("day_one_input.txt") as f:
     input = f.read().splitlines()
 
-for rotation in input:
-    distance = int(rotation[1:])
 
-    if rotation[0] == "L":
-        dial_position = (dial_position - distance) % 100
-    else:
-        dial_position = (dial_position + distance) % 100
+def part_one(input: list[str]) -> int:
+    dial_position = 50
+    secret_code = 0
+    for rotation in input:
+        distance = int(rotation[1:])
 
-    if dial_position == 0:
-        secret_code += 1
+        if rotation[0] == "L":
+            dial_position = (dial_position - distance) % 100
+        else:
+            dial_position = (dial_position + distance) % 100
+        if dial_position == 0:
+            secret_code += 1
+
+    return secret_code
 
 
-print(dial_position)
-print(secret_code)
+def part_two(input: list[str]) -> int:
+    dial_position = 50
+    secret_code = 0
+    for rotation in input:
+        distance = int(rotation[1:])
+        for _ in range(distance):
+            if rotation[0] == "L":
+                dial_position = (dial_position - 1) % 100
+            else:
+                dial_position = (dial_position + 1) % 100
+            if dial_position == 0:
+                secret_code += 1
+
+    return secret_code
+
+
+print(part_one(input))
+print(part_two(input))
